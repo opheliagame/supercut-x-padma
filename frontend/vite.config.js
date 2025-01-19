b091c6b1939d691c14807ee05332b3fff2afaeca
opheliagame <21999998+opheliagame@users.noreply.github.com>
2025-01-19 11:43:38 +0530

    config change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,19 +7,15 @@ import crossOriginIsolation from 'vite-plugin-cross-origin-isolation'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/supercut-x-padma/',
-  plugins: [
-    vue(),
-    vueJsx(),
-    crossOriginIsolation()
-  ],
- 
+  base: '/supercut-x-padma/',
+  plugins: [vue(), vueJsx(), crossOriginIsolation()],
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
   optimizeDeps: {
-    exclude: ["@ffmpeg/ffmpeg", "@ffmpeg/util"],
-},
+    exclude: ['@ffmpeg/ffmpeg', '@ffmpeg/util']
+  }
 })
