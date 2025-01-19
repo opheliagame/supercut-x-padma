b21297d7a27e5a4bb44b00d733164b374b92e81e
opheliagame <opheliagame@users.noreply.github.com>
2025-01-19 06:15:06 +0000

    deploy: 069f7a6b8f13ee864aa43dd1c17adc1e9c3f88b9

---
+++ assets/AboutView-D9uNlkYP.js
@@ -0,0 +1,1 @@
+import{c as o,a as e,o as t,_ as a,A as c}from"./index-MLawc18O.js";const s={class:"flex flex-col w-full h-full overflow-hidden"},f={__name:"AboutView",setup(l){return(_,n)=>(t(),o("div",s,[e(a),e(c)]))}};export{f as default};
